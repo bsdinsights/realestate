164debd2027af4d8189ce65b19f54ffb52aa219f
luuthinh <luuthinh2705@gmail.com>
2020-03-11 17:10:44 +0700

    DM.24 Thêm bảng phường xã

--- bsd_danh_muc/__manifest__.py
+++ bsd_danh_muc/__manifest__.py
@@ -15,6 +15,7 @@
         'views/bsd_quoc_gia_views.xml',
         'views/bsd_tinh_thanh_views.xml',
         'views/bsd_quan_huyen_views.xml',
+        'views/bsd_phuong_xa_views.xml',
         'views/bsd_menu_item_views.xml'
     ],
     'application': True,
---
+++ bsd_danh_muc/models/bsd_phuong_xa.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class BsdPhuongXa(models.Model):
+    _name = 'bsd.phuong_xa'
+    _rec_name = 'bsd_ten'
+    _description = 'Danh mục phường xã'
+
+    bsd_quoc_gia_id = fields.Many2one('bsd.quoc_gia', string="Quốc gia", required=True)
+    bsd_tinh_thanh_id = fields.Many2one('bsd.tinh_thanh', string="Tỉnh thành", required=True)
+    bsd_quan_huyen_id = fields.Many2one('bsd.quan_huyen', string="Quận huyên", required=True)
+    bsd_ten = fields.Char(string="Tên phường xã", required=True)
+    bsd_ma = fields.Char(string="Mã phường xã", required=True)
+    bsd_dien_giai = fields.Char(string="Diễn giải")
+    bsd_trang_thai = fields.Selection([('active', "Đang sử dụng"),
+                                       ('inactive', "Không sử dụng")], string='Trạng thái', default='active')
