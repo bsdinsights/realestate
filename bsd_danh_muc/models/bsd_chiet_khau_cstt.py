c8136fab534f4bbbb733a27292283bbe9d2b3530
luuthinh <34004022+luuthinh@users.noreply.github.com>
2020-03-19 15:52:07 +0700

    DM.18 Thêm view Chiết khấu theo chính sách thanh toán

--- bsd_danh_muc/models/bsd_chiet_khau_cstt.py
+++ bsd_danh_muc/models/bsd_chiet_khau_cstt.py
@@ -9,8 +9,8 @@ class BsdChietKhauCSTT(models.Model):
     _description = "Thông tin chiết khấu chính sách thanh toán"
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    bsd_ma_ck_cstt = fields.Char(string="Mã chiết khấu", help="Mã chiết khấu chính sách thanh toán", required=True)
-    bsd_ten_ck_cstt = fields.Char(string="Tên chiết khấu", help="Mã chiết khấu chính sách thanh toán", required=True)
+    bsd_ma_ck_cstt = fields.Char(string="Mã chiết khấu", help="Mã chiết khấu theo chính sách thanh toán", required=True)
+    bsd_ten_ck_cstt = fields.Char(string="Tên chiết khấu", help="Mã chiết khấu theo chính sách thanh toán", required=True)
     bsd_dien_giai = fields.Char(string="Diễn giải")
     bsd_tu_ngay = fields.Date(string="Từ ngày", help="Ngày bắt đầu áp dụng chiết khấu chung")
     bsd_den_ngay = fields.Date(string="Đến ngày", help="Ngày kết thúc áp dụng chiết khấu chung")
@@ -34,7 +34,7 @@ class BsdChietKhauCSTTChiTiet(models.Model):
     bsd_ma_ck = fields.Char(related="bsd_chiet_khau_id.bsd_ma_ck")
     bsd_tu_ngay = fields.Date(related="bsd_chiet_khau_id.bsd_tu_ngay")
     bsd_den_ngay = fields.Date(related="bsd_chiet_khau_id.bsd_den_ngay")
-    bsD_cs_tt_id = fields.Char(related="bsd_chiet_khau_id.bsd_cs_tt_id")
+    bsd_cs_tt_id = fields.Char(related="bsd_chiet_khau_id.bsd_cs_tt_id")
     bsd_cach_tinh = fields.Selection(related="bsd_chiet_khau_id.bsd_cach_tinh")
     bsd_tien_ck = fields.Monetary(related="bsd_chiet_khau_id.bsd_tien_ck")
     bsd_tl_ck = fields.Float(related="bsd_chiet_khau_id.bsd_tl_ck")
