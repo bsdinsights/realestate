d83b5f7560cd455a178abf383aeb355ce64fab4f
luuthinh <34004022+luuthinh@users.noreply.github.com>
2020-03-20 17:11:20 +0700

    DA.02 Cập nhật lại field bảng tòa nhà

--- bsd_du_an/models/bsd_toa_nha.py
+++ bsd_du_an/models/bsd_toa_nha.py
@@ -5,21 +5,21 @@ from odoo import models, fields, api
 
 class BsdBlock(models.Model):
     _name = "bsd.toa_nha"
-    _rec_name = 'bsd_ten'
+    _rec_name = 'bsd_ten_tn'
     _description = 'Thông tin tòa nhà'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    bsd_ten = fields.Char(string="Tên tòa nhà", required=True)
-    bsd_ma = fields.Char(string="Mã tòa nhà", required=True)
-    bsd_so_thu_tu = fields.Char(string="Số thứ tự sắp xếp")
+    bsd_ten_tn = fields.Char(string="Tên tòa nhà", required=True)
+    bsd_ma_tn = fields.Char(string="Mã tòa nhà", required=True)
+    bsd_stt = fields.Integer(string="Số thứ tự", help="Số thứ tự sắp xếp của tòa nhà")
     bsd_dien_giai = fields.Char(string="Diễn giải",
                                 help="Thông tin chi tiết về tòa nhà",
                                 required=True)
-    bsd_du_an_id = fields.Many2one('bsd.du_an', string="Dự án")
+    bsd_du_an_id = fields.Many2one('bsd.du_an', string="Dự án", required=True)
     bsd_so_tang = fields.Integer(string="Số tầng")
-    bsd_so_can_ho = fields.Integer(string="Số căn hộ")
+    bsd_so_unit = fields.Integer(string="Số căn hộ")
     bsd_dia_chi = fields.Text(string="Địa chỉ", help="Địa chỉ tòa nhà")
-    bsd_postal = fields.Char(string="Block Postal Code", help="Mã Postal")
-    bsd_trang_thai = fields.Selection([('active', 'Đang sử dụng'),
+    bsd_postal = fields.Char(string="Postal Code", help="Mã Postal")
+    state = fields.Selection([('active', 'Đang sử dụng'),
                                        ('inactive', 'Ngưng sử dụng')],
                                       string="Trạng thái", default='active')
