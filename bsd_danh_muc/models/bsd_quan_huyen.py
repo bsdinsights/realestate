d2a3c1cfa4f3af1b002c95b02c61317cbf738d15
luuthinh <34004022+luuthinh@users.noreply.github.com>
2020-03-28 14:46:09 +0700

    DM.23 sửa lỗi R.002 mã quận huyện là duy nhất

--- bsd_danh_muc/models/bsd_quan_huyen.py
+++ bsd_danh_muc/models/bsd_quan_huyen.py
@@ -13,7 +13,10 @@ class BsdQuanHuyen(models.Model):
     bsd_tinh_thanh_id = fields.Many2one('res.country.state', string="Tỉnh thành",
                                         required=True)
     bsd_ten = fields.Char(string="Tên quận huyện", required=True)
-    bsd_ma = fields.Char(string="Mã quận huyện", required=True)
+    bsd_ma = fields.Char(string="Mã quận huyện", required=True, copy=False)
     bsd_dien_giai = fields.Char(string="Diễn giải")
     state = fields.Selection([('active', "Đang sử dụng"),
                                        ('inactive', "Không sử dụng")], string='Trạng thái', default='active')
+    _sql_constraints = [
+        ('bsd_ma_unique', 'unique(bsd_ma)', 'Mã đã được sử dung')
+    ]
