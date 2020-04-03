35079a813c0c45f1982c2c7360a1a3160d460c01
luuthinh <34004022+luuthinh@users.noreply.github.com>
2020-04-03 13:50:46 +0700

    Thêm field trạng thái bảng tài khoản ngân hàng

--- bsd_danh_muc/models/res_country.py
+++ bsd_danh_muc/models/res_country.py
@@ -17,3 +17,11 @@ class ResCountryState(models.Model):
     state = fields.Selection([('active', "Đang sử dụng"),
                               ('inactive', "Không sử dụng")], string='Trạng thái',
                              default='active', tracking=1, help="Trạng thái")
+
+
+class ResPartnerBank(models.Model):
+    _inherit = 'res.partner.bank'
+
+    state = fields.Selection([('active', "Đang sử dụng"),
+                              ('inactive', "Không sử dụng")], string='Trạng thái',
+                             default='active', tracking=1, help="Trạng thái")
