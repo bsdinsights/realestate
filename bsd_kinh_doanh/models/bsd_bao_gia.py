87415e4629e7a2af7e2931fc0bfc1f7316549454
hungpqbsd <hungpq@bsdinsight.com>
2020-04-11 15:52:49 +0700

    KD.09.07 hủy báo giá

--- bsd_kinh_doanh/models/bsd_bao_gia.py
+++ bsd_kinh_doanh/models/bsd_bao_gia.py
@@ -285,3 +285,14 @@ class BsdBaoGia(models.Model):
     def action_ky_bg(self):
         action = self.env.ref('bsd_kinh_doanh.bsd_wizard_ky_bg_action').read()[0]
         return action
+
+    # KD.09.07 Hủy báo giá
+    def action_huy(self):
+        hop_dong = self.env['bsd.hd_ban'].search([('state', '!=', 'huy'), ('bsd_bao_gia_id', '=', self.id)])
+        if hop_dong:
+            raise UserError("Đã có phát sinh Hợp đồng. Bạn không thể Báo giá")
+        else:
+            self.write({
+                'state': 'huy',
+            })
+
