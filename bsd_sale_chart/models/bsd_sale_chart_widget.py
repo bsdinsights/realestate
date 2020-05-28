49e585f8b7e4da5572f613ad678e3468c9a91187
luuthinh <34004022+luuthinh@users.noreply.github.com>
2020-05-28 16:44:23 +0700

    Cập nhật module sale chart

--- bsd_sale_chart/models/bsd_sale_chart_widget.py
+++ bsd_sale_chart/models/bsd_sale_chart_widget.py
@@ -19,16 +19,49 @@ class BsdSaleChartWidget(models.AbstractModel):
             raise UserError("Vui lòng điền trường dự án")
         self.env.cr.execute(
             """
+        WITH price AS (WITH max_pricelist AS (SELECT item.id,item.product_tmpl_id,item.fixed_price 
+            from product_pricelist_item AS item
+                LEFT JOIN product_pricelist AS pricelist ON pricelist.id = item.pricelist_id 
+                JOIN (SELECT 
+                item.product_tmpl_id AS unit,
+                max(pricelist.create_date) AS max_date
+            FROM product_pricelist_item AS item
+                LEFT JOIN product_pricelist AS pricelist ON pricelist.id = item.pricelist_id
+            GROUP BY item.product_tmpl_id) AS max_item ON max_item.unit = item.product_tmpl_id 
+            AND pricelist.create_date = max_item.max_date)
+        SELECT 
+            unit.id AS unit_id,
+            CASE
+                WHEN dot_mb.id IS NOT NULL AND item.fixed_price IS NOT NULL THEN item.fixed_price
+                WHEN max_item.id IS NOT NULL AND max_item.fixed_price IS NOT NULL THEN max_item.fixed_price          
+                ELSE unit.bsd_tong_gb
+                END 
+                AS chot_gia
+                FROM product_template AS unit
+                    LEFT JOIN bsd_dot_mb AS dot_mb ON dot_mb.id = unit.bsd_dot_mb_id
+                    LEFT JOIN product_pricelist AS pricelist ON pricelist.id = dot_mb.bsd_bang_gia_id
+                    LEFT JOIN product_pricelist_item AS item ON item.pricelist_id = pricelist.id and item.product_tmpl_id = unit.id
+                    LEFT JOIN max_pricelist AS max_item ON max_item.product_tmpl_id = unit.id)
                 SELECT 
-                    toa.id,toa.bsd_ten_tn,
-                    tang.id,tang.bsd_ma_tang,
-                    unit.id,unit.name,unit.state  
-                FROM 
-                    bsd_toa_nha AS toa
-                LEFT JOIN 
-                    bsd_tang AS tang ON toa.id = tang.bsd_toa_nha_id
-                LEFT JOIN 
-                    product_template AS unit ON tang.id = unit.bsd_tang_id
+                    toa.id AS id_toa,
+                    toa.bsd_ten_tn,
+                    tang.id AS id_tang,
+                    tang.bsd_ma_tang,
+                    unit.id AS id_unit,
+                    unit.name,
+                    unit.state, 
+                    giu_cho.so_giu_cho_unit,
+                    price.chot_gia AS gia_ban
+                FROM bsd_toa_nha AS toa
+                LEFT JOIN bsd_tang AS tang 
+                    ON toa.id = tang.bsd_toa_nha_id
+                LEFT JOIN product_template AS unit 
+                    ON tang.id = unit.bsd_tang_id
+                LEFT JOIN (SELECT unit.product_tmpl_id,COUNT(*) AS so_giu_cho_unit 
+                                            FROM bsd_giu_cho AS giu_cho
+                                            LEFT JOIN product_product AS unit ON unit.id = giu_cho.bsd_unit_id
+                                            GROUP BY unit.product_tmpl_id) AS giu_cho ON giu_cho.product_tmpl_id = unit.id
+                LEFT JOIN price ON price.unit_id = unit.id
                 WHERE 
                     (toa.bsd_du_an_id = %s)
                 ORDER BY
--- bsd_sale_chart/static/js/bsd_sale_chart_renderer.js
+++ bsd_sale_chart/static/js/bsd_sale_chart_renderer.js
@@ -88,17 +88,57 @@ odoo.define('bsd_sale_chart.SaleChartRenderer', function(require){
                     args: [self.filter],
                     context: self.context,
             }).then(function (data){
-                var group_toa = _.groupBy(data, function(data) { return data[0]});
+                var format_curency = ((money) => {
+                        money = money.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
+                        return money;
+                });
+                var data_filter = _.each(data, function(item,index,data){
+                    if (item[7] === null){
+                        item[7] = 0
+                    }
+                    if (item[8] !== null){
+                        item[8] = format_curency(item[8])
+                    }
+                })
+                var group_toa = _.groupBy(data, function(data_filter) { return data_filter[0]});
                 console.log(group_toa)
                 _.each(group_toa, function(item,index,group_toa){
                     var toa = {};
                     toa.headerToa = [item[0][0],item[0][1]]
+                    toa.state={
+                            chuan_bi:0,
+                            san_sang:0,
+                            dat_cho:0,
+                            giu_cho:0,
+                            dat_coc:0,
+                            chuyen_coc:0,
+                            da_thu_coc:0,
+                            hoan_tat_dat_coc:0,
+                            thanh_toan_dot_1:0,
+                            ky_thoa_thuan_coc:0,
+                            du_dieu_kien:0,
+                            da_ban:0,
+                        }
                     var group_tang = _.groupBy(item, function(item){ return item[2]})
                     var k = [];
                     _.each(group_tang, function(item,index,group_tang){
                         var tang ={};
                         tang.headerTang=[item[0][2],item[0][3]]
                         tang.detailTang=item
+                        tang.state =_.countBy(item,function(item){
+                            if (item[6] == 'chuan_bi') {return 'chuan_bi'}
+                            if (item[6] == 'san_sang') {return 'san_sang'}
+                            if (item[6] == 'dat_cho') {return 'dat_cho'}
+                            if (item[6] == 'giu_cho') {return 'giu_cho'}
+                            if (item[6] == 'dat_coc') {return 'dat_coc'}
+                            if (item[6] == 'chuyen_coc') {return 'chuyen_coc'}
+                            if (item[6] == 'da_thu_coc') {return 'da_thu_coc'}
+                            if (item[6] == 'hoan_tat_dat_coc') {return 'hoan_tat_dat_coc'}
+                            if (item[6] == 'thanh_toan_dot_1') {return 'thanh_toan_dot_1'}
+                            if (item[6] == 'ky_thoa_thuan_coc') {return 'ky_thoa_thuan_coc'}
+                            if (item[6] == 'du_dieu_kien') {return 'du_dieu_kien'}
+                            if (item[6] == 'da_ban') {return 'da_ban'}
+                        })
                         k.push(tang)
                     })
                     toa.detailToa= k
