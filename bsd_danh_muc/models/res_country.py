a93320e72a0b6ca46fcbb663b1b212fa2df177ff
luuthinh <34004022+luuthinh@users.noreply.github.com>
2020-06-08 14:53:07 +0700

    Gợi ý địa lý

--- bsd_danh_muc/models/res_country.py
+++ bsd_danh_muc/models/res_country.py
@@ -5,7 +5,9 @@ from odoo import models, fields, api
 
 class ResCountry(models.Model):
     _inherit = 'res.country'
+    _order = 'sequence ASC'
 
+    sequence = fields.Integer(string="Thứ tự")
     state = fields.Selection([('active', "Đang sử dụng"),
                               ('inactive', "Không sử dụng")], string='Trạng thái',
                              default='active', tracking=1, help="Trạng thái")
@@ -13,7 +15,9 @@ class ResCountry(models.Model):
 
 class ResCountryState(models.Model):
     _inherit = 'res.country.state'
+    _order = 'sequence ASC'
 
+    sequence = fields.Integer(string="Thứ tự")
     state = fields.Selection([('active', "Đang sử dụng"),
                               ('inactive', "Không sử dụng")], string='Trạng thái',
                              default='active', tracking=1, help="Trạng thái")
