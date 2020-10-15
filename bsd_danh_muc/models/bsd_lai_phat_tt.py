83757f5a350a7bef75d4b27b1d5035372a26cd64
luuthinh <34004022+luuthinh@users.noreply.github.com>
2020-10-15 11:04:28 +0700

    Sửa lỗi lãi phạt chậm thanh toán'

--- bsd_danh_muc/models/bsd_lai_phat_tt.py
+++ bsd_danh_muc/models/bsd_lai_phat_tt.py
@@ -1,27 +1,29 @@
 # -*- coding:utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
 
 
 class BsdLaiPhatTT(models.Model):
     _name = 'bsd.lai_phat_tt'
-    _rec_name = 'bsd_ten_lptt'
-    _description = "Thông tin lãi phạt thanh toán"
+    _rec_name = 'bsd_ma_lptt'
+    _description = "Thông tin lãi phạt chậm thanh toán"
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    bsd_ma_lptt = fields.Char(string="Mã", help="Mã lãi phạt thanh toán", required=True)
+    bsd_ma_lptt = fields.Char(string="Mã", help="Mã lãi phạt chậm thanh toán", required=True, readonly=True, copy=False,
+                            default='/')
     _sql_constraints = [
         ('bsd_ma_lptt_unique', 'unique (bsd_ma_lptt)',
          'Mã lãi phạt thanh toán đã tồn tại !'),
     ]
     bsd_ten_lptt = fields.Char(string="Tên", help="Tên lãi phạt thanh toán", required=True)
     bsd_dien_giai = fields.Char(string="Diễn giải", help="Diễn giải")
-    bsd_du_an_id = fields.Many2one('bsd.du_an', string="Dự án", help="Tên dự án")
+    bsd_du_an_id = fields.Many2one('bsd.du_an', string="Dự án", help="Tên dự án", required=True)
     bsd_tinh_phat = fields.Selection([('htt', 'Hạn thanh toán'), ('nah', "Ngày ân hạn")],
-                                     string="Tính phạt", default="htt",
+                                     string="Phương thức tính phạt", default="htt",
                                      help="""
-                                        Quy định tính lại phạt từ ngày hết hạn thanh toán hay kể từ ngày ân hạn
-                                     """)
+                                        Quy định tính lãi phạt chậm thanh toán từ ngày hết hạn thanh toán hay kể từ ngày ân hạn
+                                     """, required=True)
     bsd_an_han = fields.Integer(string="Ân hạn", required=True,
                                 help="""
                                     Số ngày ân hạn tính lãi phạt. Nếu khách hàng đóng tiền trong thời gian ân hạn, 
@@ -35,3 +37,20 @@ class BsdLaiPhatTT(models.Model):
                              string="Trạng thái", default='active', required=True, tracking=1, help="Trạng thái")
     company_id = fields.Many2one('res.company', string='Công ty', default=lambda self: self.env.company)
     currency_id = fields.Many2one(related="company_id.currency_id", string="Tiền tệ", readonly=True)
+
+    def name_get(self):
+        res = []
+        for lt in self:
+            res.append((lt.id, lt.bsd_ten_lptt))
+        return res
+
+    @api.model
+    def create(self, vals):
+        sequence = False
+        if 'bsd_du_an_id' in vals:
+            du_an = self.env['bsd.du_an'].browse(vals['bsd_du_an_id'])
+            sequence = du_an.get_ma_bo_cn(loai_cn=self._name)
+        if not sequence:
+            raise UserError(_('Dự án chưa có mã lãi phạt chậm thanh toán.'))
+        vals['bsd_ma_lptt'] = sequence.next_by_id()
+        return super(BsdLaiPhatTT, self).create(vals)
--- bsd_danh_muc/models/bsd_ma_cn.py
+++ bsd_danh_muc/models/bsd_ma_cn.py
@@ -13,6 +13,7 @@ class BsdMaBoChungTu(models.Model):
     bsd_du_an_id = fields.Many2one('bsd.du_an', string="Dự án", help="Dự án")
     bsd_loai_cn = fields.Selection([('bsd.du_an', 'Dự án'),
                                     ('product.pricelist', 'Bảng giá'),
+                                    ('bsd.lai_phat_tt', 'Lãi phạt chậm thanh toán'),
                                     ('bsd.gc_tc', 'Giữ chỗ thiện chí'),
                                     ('bsd.rap_can', 'Ráp căn'),
                                     ('bsd.giu_cho', 'Giữ chỗ'),
