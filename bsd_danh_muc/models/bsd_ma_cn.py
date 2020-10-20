d9240ebaa3c999838bb10da1bde9eb37b9abbafa
luuthinh <34004022+luuthinh@users.noreply.github.com>
2020-10-20 16:48:09 +0700

    sửa lỗi mã chứng từ

--- bsd_danh_muc/models/bsd_ma_cn.py
+++ bsd_danh_muc/models/bsd_ma_cn.py
@@ -112,3 +112,14 @@ class BsdMaBoChungTu(models.Model):
         if not vals.get('bsd_ma_tt_id'):
             vals.update({'bsd_ma_tt_id': self.sudo()._create_sequence(vals).id})
         return super(BsdMaBoChungTu, self).create(vals)
+
+    def write(self, vals):
+        if 'bsd_ma_cn' in vals.keys():
+            self.bsd_ma_tt_id.write({
+                'prefix': vals['bsd_ma_cn'],
+            })
+        if 'bsd_sl_ky_tu' in vals.keys():
+            self.bsd_ma_tt_id.write({
+                'padding': vals['bsd_sl_ky_tu'],
+            })
+        return super(BsdMaBoChungTu, self).write(vals)
