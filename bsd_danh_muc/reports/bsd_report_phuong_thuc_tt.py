fe01a278a73816ef3fd97b8b2016b94842ffca0c
luuthinh <34004022+luuthinh@users.noreply.github.com>
2020-10-24 14:36:55 +0700

    sửa tính năng in xem trước lịch thanh toán 2

--- bsd_danh_muc/reports/bsd_report_phuong_thuc_tt.py
+++ bsd_danh_muc/reports/bsd_report_phuong_thuc_tt.py
@@ -25,20 +25,12 @@ class ReportLTT(models.AbstractModel):
         dot_tt_ids = cs_tt.bsd_ct_ids
         # biến chứa lịch thanh toán
         list_ltt = []
-        check_dot_ky_hd = False
-        check_dot_cuoi = False
-        check_dot_pql = False
-        check_dot_pbt = False
         # Tạo các đợt thanh toán
         for dot in dot_tt_ids.sorted('bsd_stt'):
             # Tạo dữ liệu đợt cố định
             if dot.bsd_cach_tinh == 'cd' and not dot.bsd_dot_cuoi:
                 dot_cd = dot
                 stt += 1
-                check_dot_ky_hd = dot_cd.bsd_dot_ky_hd
-                check_dot_cuoi = dot_cd.bsd_dot_cuoi
-                check_dot_pbt = dot_cd.bsd_tinh_pbt
-                check_dot_pql = dot_cd.bsd_tinh_pql
                 # Ngày hết hạn thanh toán
                 ngay_hh_tt = dot_cd.bsd_ngay_cd
                 # Tỷ lệ thanh toán của đợt
@@ -46,10 +38,10 @@ class ReportLTT(models.AbstractModel):
                     'Đợt {0}'.format(stt),
                     ngay_hh_tt.strftime("%d/%m/%y"),
                     '{0} %'.format(dot_cd.bsd_tl_tt).replace('.', ','),
-                    check_dot_ky_hd,
-                    check_dot_cuoi,
-                    check_dot_pql,
-                    check_dot_pbt
+                    dot_cd.bsd_dot_ky_hd,
+                    dot_cd.bsd_dot_cuoi,
+                    dot_cd.bsd_tinh_pql,
+                    dot_cd.bsd_tinh_pbt
                 ))
             # Tạo dữ liệu đợt tự động
             elif dot.bsd_cach_tinh == 'td':
@@ -75,10 +67,6 @@ class ReportLTT(models.AbstractModel):
                 ngay_hh_tt = list_ngay_hh_tt_td[-1]
                 # kiểm tra đợt đầu tiên tự động
                 stt_td_đau_tien = stt + 1
-                check_dot_ky_hd = dot_td.bsd_dot_ky_hd
-                check_dot_cuoi = dot_td.bsd_dot_cuoi
-                check_dot_pbt = dot_td.bsd_tinh_pbt
-                check_dot_pql = dot_td.bsd_tinh_pql
                 # Gán lich
                 for ngay in list_ngay_hh_tt_td:
                     stt += 1
@@ -94,10 +82,10 @@ class ReportLTT(models.AbstractModel):
                             'Đợt {0}'.format(stt),
                             ngay.strftime("%d/%m/%y"),
                             '{0} %'.format(dot_td.bsd_tl_tt).replace('.', ','),
-                            check_dot_ky_hd,
-                            check_dot_cuoi,
-                            check_dot_pql,
-                            check_dot_pbt
+                            dot_td.bsd_dot_ky_hd,
+                            dot_td.bsd_dot_cuoi,
+                            dot_td.bsd_tinh_pql,
+                            dot_td.bsd_tinh_pbt
                         ))
                     else:
                         list_ltt.append((
@@ -115,10 +103,6 @@ class ReportLTT(models.AbstractModel):
                 dot_dkbg = dot
                 ngay_hh_tt_dkbg = cs_tt.bsd_du_an_id.bsd_ngay_dkbg or False
                 stt += 1
-                check_dot_ky_hd = dot_dkbg.bsd_dot_ky_hd
-                check_dot_cuoi = dot_dkbg.bsd_dot_cuoi
-                check_dot_pbt = dot_dkbg.bsd_tinh_pbt
-                check_dot_pql = dot_dkbg.bsd_tinh_pql
                 # cập nhật lại ngày hết hạn thanh toán
                 ngay_hh_tt = ngay_hh_tt_dkbg
 
@@ -126,28 +110,24 @@ class ReportLTT(models.AbstractModel):
                     'Đợt {0}'.format(stt),
                     ngay_hh_tt.strftime("%d/%m/%y"),
                     '{0} %'.format(dot_dkbg.bsd_tl_tt).replace('.', ','),
-                    check_dot_ky_hd,
-                    check_dot_cuoi,
-                    check_dot_pql,
-                    check_dot_pbt
+                    dot_dkbg.bsd_dot_ky_hd,
+                    dot_dkbg.bsd_dot_cuoi,
+                    dot_dkbg.bsd_tinh_pql,
+                    dot_dkbg.bsd_tinh_pbt
                 ))
 
             # Tạo đợt thanh toán cuối
             elif dot.bsd_dot_cuoi:
                 dot_cuoi = dot
                 stt += 1
-                check_dot_ky_hd = dot_cuoi.bsd_dot_ky_hd
-                check_dot_cuoi = dot_cuoi.bsd_dot_cuoi
-                check_dot_pbt = dot_cuoi.bsd_tinh_pbt
-                check_dot_pql = dot_cuoi.bsd_tinh_pql
                 list_ltt.append((
                     'Đợt {0}'.format(stt),
                     None,
                     '{0} %'.format(dot_cuoi.bsd_tl_tt).replace('.', ','),
-                    check_dot_ky_hd,
-                    check_dot_cuoi,
-                    check_dot_pql,
-                    check_dot_pbt
+                    dot_cuoi.bsd_dot_ky_hd,
+                    dot_cuoi.bsd_dot_cuoi,
+                    dot_cuoi.bsd_tinh_pql,
+                    dot_cuoi.bsd_tinh_pbt
                 ))
         return list_ltt
 
@@ -164,6 +144,4 @@ class ReportLTT(models.AbstractModel):
             'ngay_ht': ngay_hien_tai.strftime("%d/%m/%y"),
             'list_tt': self._tao_lich_tt(cs_tt, ngay_hien_tai)
         }
-        _logger.debug("kết quả in")
-        _logger.debug(res)
         return res
